d9e914f76cc05773a130bb7ab4eff03e617963f7
bdrkreza <diureza@gamil.com>
2021-02-07 09:41:36 +0600

    update

---
+++ assets/apps.js
@@ -0,0 +1,38 @@
+fetch("https://restcountries.eu/rest/v2/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+
+const displayCountries = (countries) => {
+    const countriesDiv = document.getElementById("countries");
+    countries.forEach((country) => {
+        const countryDiv = document.createElement("div");
+        countriesDiv.appendChild(countryDiv);
+        countryDiv.className = "country";
+
+        const countryInfo = `
+<h3 class="country-name">${country.name}</h3>
+<p class="country-capital">${country.capital}</p>
+<button onclick="displayCountryDetail('${country.name}')" >Details</button>
+`;
+        countryDiv.innerHTML = countryInfo;
+    });
+};
+
+const displayCountryDetail = (name) => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => renderCountryInfo(data[0]));
+
+    const renderCountryInfo = (country) => {
+        console.log(country);
+        const countryDiv = document.getElementById("countryDetail");
+        countryDiv.innerHTML = `
+    <h1>${country.name}</h1>
+    <p>${country.population}</p>
+    <p>${country.area}</p>
+    <img class="flag" src ="${country.flag}">
+    
+    `;
+    };
+};
