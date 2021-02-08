721884ec3646ad467bab42fbac85c9e2b1951bd9
bdrkreza <diureza@gamil.com>
2021-02-08 23:56:25 +0600

    update

--- assets/apps.js
+++ assets/apps.js
@@ -1,29 +1,50 @@
-const searchFoods = () => {
-    const searchText = document.getElementById('search-field').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayFoods(data.meals))
-}
+// id call 
+const searchInput = document.getElementById('search-field');
+const searchBtn = document.getElementById('search-btn');
+const searchFoods = document.getElementById('foods');
+const searchResult = document.getElementById('search-result');
+// const singleFoods = document.getElementById('single-foods');
 
-const displayFoods = foods => {
-    // console.log(foods);
-    const foodContainer = document.getElementById("food-container");
-    foodContainer.innerHTML = "";
-    foods.forEach(food => {
-        const foodDiv = document.createElement('div');
-        foodDiv.className = "col-lg-3 d-flex justify-content-between mx-auto p-3 py-4"
-        foodDiv.innerHTML = `
-        <div class="card" style="width: 18rem">
-            <img onclick="singleFoods('${food.strMeal}')" class="card-img-top" src="${food.strMealThumb}" alt="Card image cap " />
-        <div class="card-body">
-            <h3 class="card-text">${food.strMeal}</h3>
-        </div>`;
-        foodContainer.appendChild(foodDiv);
-    })
-}
+//event listener handle
+searchBtn.addEventListener('click', getFoodsList);
 
-// get recipe of the meal
+// Search Foods
+function getFoodsList(Element) {
+    Element.preventDefault();
+
+    const inputData = (searchInput.value);
+    if (inputData.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputData}`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                searchResult.innerHTML = `<h2> search result for ${inputData} </h2>`;
+                if (data.meals === null) {
+                    searchResult.innerHTML = `<h3> there are no Result for ${inputData}</h3>`;
+                } else {
+                    const foodContainer = document.getElementById("food-container");
+                    searchFoods.innerHTML = data.meals.map(
+                        (food) => {
+
+                            const foodDiv = document.createElement('div');
+                            foodDiv.className = "col-lg-3 d-flex justify-content-between mx-auto p-3 py-4"
+                            foodDiv.innerHTML = `
+                            <div class="card" style="width: 18rem">
+                                <img onclick="singleFoods('${food.strMeal}')" class="card-img-top" src="${food.strMealThumb}" alt="Card image cap " />
+                            <div class="card-body">
+                                <h3 class="card-text">${food.strMeal}</h3>
+                            </div>`;
+                            foodContainer.appendChild(foodDiv);
+                        })
+                }
+            })
+
+
+    } else {
+        alert("Please enter a search Foods");
+    }
+
+}
 
 const singleFoods = (name) => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
